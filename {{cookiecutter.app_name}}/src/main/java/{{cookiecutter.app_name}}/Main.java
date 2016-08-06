66fb075837873583b9a036fa44f6afd4e2e6e433
Tengfei Yang <hzyangtengfei@corp.netease.com>
2016-08-06 11:14:56 +0800

    add web starter

--- {{cookiecutter.app_name}}/src/main/java/{{cookiecutter.app_name}}/Main.java
+++ {{cookiecutter.app_name}}/src/main/java/{{cookiecutter.app_name}}/Main.java
@@ -1,7 +1,19 @@
-package {{cookiecutter.app_name}}
+package {{cookiecutter.app_name}};
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("hello world");
-	}
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
+    }
 }
